9d1d435d157c3c0f19ce8eaa1da18576dc43e4c5
JustD1d1t <starzyk.dawid.0826@gmail.com>
2024-07-08 19:02:40 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const links = [{
+    label: 'Home',
+    icon: 'i-heroicons-home'
+}, {
+    label: 'Inbox',
+    icon: 'i-heroicons-inbox',
+    badge: '4'
+}, {
+    label: 'Users',
+    icon: 'i-heroicons-user-group'
+}, {
+    label: 'Settings',
+    icon: 'i-heroicons-cog-8-tooth',
+    children: [{
+        label: 'General'
+    }, {
+        label: 'Members'
+    }, {
+        label: 'Notifications'
+    }]
+}]
+</script>
+<template>
+    <UDashboardLayout>
+        <UDashboardPanel :width="250" >
+            <UDashboardNavbar>
+                <template #left>
+                    <Logo class="w-auto h-5" />
+                </template>
+            </UDashboardNavbar>
+
+            <UDashboardSidebar>
+                <UDashboardSidebarLinks :links="links" />
+            </UDashboardSidebar>
+        </UDashboardPanel>
+
+        <slot />
+    </UDashboardLayout>
+</template>
